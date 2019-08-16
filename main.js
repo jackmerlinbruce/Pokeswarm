642876a839c44644be8f505c7a3dcb5c6fe57150
Jack Merlin Bruce <j.merlin.b@gmail.com>
2019-08-16 19:18:27 +0100

    final changes

--- main.js
+++ main.js
@@ -20,32 +20,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (axisFilter === 'is_legendary') {
             return {
-                'x': d3.scaleLinear().range([padding, width - margin]).domain([-1, max + 1]),
-                'y': d3.scaleLinear().range([height - padding, padding]).domain([-1, max + 1]),
+                'x': d3.scaleBand().rangeRound(xRange).domain(uniques),
+                'y': d3.scaleBand().rangeRound(yRange).domain(uniques),
                 'r': d3.scaleLinear().range([spriteSize * 0.75, spriteSize * 1.5]).domain([min, max]),
-                'c': d3.scaleOrdinal(d3.schemeCategory10)
+                'c': d3.scaleOrdinal(d3.schemeCategory10),
+                'offsetScaleBandx': ((width - padding) / uniques.length) / 2,
+                'offsetScaleBandy': ((height - padding) / uniques.length) / 2
+                // 'offsetScaleBandy': 
             }
         } else if (axisFilter === 'generation') {
             return {
-                'x': d3.scaleBand().rangeRound([padding, width - margin]).domain(uniques),
-                'y': d3.scaleLinear().range([height - padding, padding]).domain([0, max + 1]),
+                'x': d3.scaleBand().rangeRound(xRange).domain(uniques),
+                'y': d3.scaleBand().rangeRound(yRange).domain(uniques),
                 'r': d3.scaleLinear().range([spriteSize * 0.75, spriteSize * 1.5]).domain([min, max]),
-                'c': d3.scaleSequential(d3.interpolatePlasma).domain([0, max])
+                'c': d3.scaleSequential(d3.interpolatePlasma).domain([0, max]),
+                'offsetScaleBandx': ((width - padding) / uniques.length) / 2,
+                'offsetScaleBandy': ((height - padding) / uniques.length) / 2
             }
         } else if ((axisFilter === 'type1') || (axisFilter === 'type2')) {
             let i = Array.from(Array(uniques.length).keys())
             return {
-                'x': d3.scaleBand().rangeRound([padding, width - margin]).domain(uniques.sort()),
-                'y': d3.scaleBand().rangeRound([height - padding, padding]).domain(uniques.sort()),
+                'x': d3.scaleBand().rangeRound(xRange).domain(uniques.sort()),
+                'y': d3.scaleBand().rangeRound(yRange).domain(uniques.sort()),
                 'r': d3.scaleLinear().range([spriteSize, spriteSize]).domain([spriteSize, spriteSize]),
-                'c': d3.scaleOrdinal(d3.schemeCategory10)
+                'c': d3.scaleOrdinal(d3.schemeCategory10),
+                'offsetScaleBandx': ((width - padding) / uniques.length) / 2,
+                'offsetScaleBandy': ((height - padding) / uniques.length) / 2
             }
         } else {
             return {
-                'x': d3.scaleLinear().range([padding, width - margin]).domain([0, max + 1]),
-                'y': d3.scaleLinear().range([height - padding, padding]).domain([0, max + 1]),
+                'x': d3.scaleLinear().range(xRange).domain([0, max + (max * 0.05)]),
+                'y': d3.scaleLinear().range(yRange).domain([0, max + (max * 0.05)]),
                 'r': d3.scaleLinear().range([spriteSize * 0.75, spriteSize * 1.5]).domain([min, max]),
-                'c': d3.scaleSequential(d3.interpolatePlasma).domain([0, max + 1])
+                'c': d3.scaleSequential(d3.interpolatePlasma).domain([0, max + 1]),
+                'offsetScaleBandx': 0,
+                'offsetScaleBandy': 0
             }
         }
     }
@@ -54,11 +63,30 @@ document.addEventListener('DOMContentLoaded', () => {
     // SET UP BUTTONS
     ///////////////////////
 
-    let stats = [
+    const stats = [
         'attack', 'defense', 'sp_attack', 'sp_defense', 'speed', 'hp',
         'weight_kg', 'height_m', 'is_legendary', 'generation', 'type1', 'type2',
-        'base_total', 'base_egg_steps', 'against_dragon', 'against_fairy', 'experience_growth'
+        'base_total', 'base_egg_steps', 'experience_growth'
     ]
+
+    const statMap = {
+        'attack' : 'Attack',
+        'defense' : 'Defence',
+        'sp_attack' : 'Special Attack',
+        'sp_defense' : 'Special Defence',
+        'speed' : 'Speed',
+        'hp' : 'HP',
+        'base_total' : 'Base Total',
+        'base_egg_steps' : 'Base Egg Steps',
+        'experience_growth' : 'Experience Growth',
+        'weight_kg' : 'Weight (Kg)',
+        'height_m' : 'Height (m)',
+        'type1' : 'Primary Type',
+        'type2' : 'Secondary Type',
+        'is_legendary' : 'Is Legendary?',
+        'generation' : 'Generation'
+         
+    }
 
     let dropdown = {
         'x': document.getElementById('select-x-axis'),
@@ -66,8 +94,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     stats.forEach(stat => {
-        dropdown.x.innerHTML += `<option value="${stat}">${stat}</option>`
-        dropdown.y.innerHTML += `<option value="${stat}">${stat}</option>`
+        dropdown.x.innerHTML += `<option value="${stat}">${statMap[stat]}</option>`
+        dropdown.y.innerHTML += `<option value="${stat}">${statMap[stat]}</option>`
     })
 
     ///////////////////////
@@ -75,14 +103,17 @@ document.addEventListener('DOMContentLoaded', () => {
     ///////////////////////
 
     let width = window.innerWidth,
-        height = window.innerHeight * 0.75 //* 2
-        margin = 200,
-        padding = 20,
-        radius = 4;
-        spriteSize = 30
+        height = window.innerHeight, //* 2
+        margin = 0,
+        padding = 50,
+        radius = 4,
+        spriteSize = 30,
+        xRange = [padding * 2, width - (padding * 2)],
+        yRange = [height - padding, padding]
+
 
     let svg = d3.select('.canvas').append('svg')
-        .attr('width', width - margin)
+        .attr('width', width)
         .attr('height', height)
 
     d3.json('pokemon.json', function (data) {
@@ -97,23 +128,28 @@ document.addEventListener('DOMContentLoaded', () => {
         svg.append("g")
             .classed('x-scale', true)
             .attr("transform", `translate(0, ${height - padding})`)
-            .call(d3.axisBottom(scales.x))
+            .call(d3.axisBottom(scales.x).tickSize(-(height - (padding * 2))))
+            .attr('visibility', 'hidden')
         svg.append("g")
             .classed('y-scale', true)
-            .attr("transform", `translate(${padding}, 0)`)
-            .call(d3.axisLeft(scales.y))
-
+            .attr("transform", `translate(${padding * 2}, 0)`)
+            .call(d3.axisLeft(scales.y).tickSize(-(width - (padding * 4))))
+            .attr('visibility', 'hidden')
+        
         // LABELS
         svg.append('text')
             .classed('axis-label x-axis-label', true)
-            .text(axisFilter.toUpperCase())
+            .text(statMap[axisFilter].toUpperCase())
             .attr('x', width / 2 - padding)
             .attr('y', height - 2)
+            .attr('visibility', 'hidden')
         svg.append('text')
             .classed('axis-label y-axis-label', true)
             .text('defense'.toUpperCase())
-            .attr('x', padding + 50)
+            .attr('x', padding)
             .attr('y', height / 2)
+            .attr('transform', `rotate(270,${padding},${height / 2})`)
+            .attr('visibility', 'hidden')
 
         // BEES
         svg.selectAll('image')
@@ -122,27 +158,27 @@ document.addEventListener('DOMContentLoaded', () => {
             .attr('width', spriteSize)
             .attr('height', spriteSize)
             // .attr('xlink:href', function (d) { return `https://img.pokemondb.net/sprites/red-blue/normal/${d.name.toLowerCase()}.png` })
-            .attr('xlink:href', function (d) { return `https://img.pokemondb.net/sprites/sun-moon/icon/${d.name.toLowerCase()}.png`})
-            .attr('x', (d) => { return scales.x(d[axisFilter]) })
-            .attr('y', (d) => { return scales.y(d['defense']) })
+            .attr('xlink:href', function (d) { return `./pokemon/${d.name.toLowerCase()}.png`})
+            .attr('x', (d) => { return scales.x(d[axisFilter]) + scales.offsetScaleBandx })
+            .attr('y', (d) => { return scales.y(d['defense']) + scales.offsetScaleBandy })
             .on('mouseover', function (d) {
                 // Guidelines
-                svg.append('line')
-                    .classed('pokemon-name', true)
-                    .attr('x1', padding)
-                    .attr('x2', scales.x(d[axisFilter]))
-                    .attr('y1', scales.y(d['defense']))
-                    .attr('y2', scales.y(d['defense']))
-                    .attr('stroke', 'black')
-                    .attr('stroke-opacity', 0.2)
-                svg.append('line')
-                    .classed('pokemon-name', true)
-                    .attr('x1', scales.x(d[axisFilter]))
-                    .attr('x2', scales.x(d[axisFilter]))
-                    .attr('y1', height - padding)
-                    .attr('y2', scales.y(d['defense']))
-                    .attr('stroke', 'black')
-                    .attr('stroke-opacity', 0.2)
+                // svg.append('line')
+                //     .classed('pokemon-name', true)
+                //     .attr('x1', padding)
+                //     .attr('x2', scales.x(d[axisFilter]))
+                //     .attr('y1', scales.y(d['defense']))
+                //     .attr('y2', scales.y(d['defense']))
+                //     .attr('stroke', 'black')
+                //     .attr('stroke-opacity', 0.2)
+                // svg.append('line')
+                //     .classed('pokemon-name', true)
+                //     .attr('x1', scales.x(d[axisFilter]))
+                //     .attr('x2', scales.x(d[axisFilter]))
+                //     .attr('y1', height - padding)
+                //     .attr('y2', scales.y(d['defense']))
+                //     .attr('stroke', 'black')
+                //     .attr('stroke-opacity', 0.2)
 
                 // Tool Tip
                 svg.append('rect')
@@ -209,61 +245,104 @@ document.addEventListener('DOMContentLoaded', () => {
 
         let simulation = d3.forceSimulation(data)
             .force('x', d3.forceX(function (d) {
-                return scales.x(d[axisFilter])
-            }).strength(0.99))
+                return width / 2
+            }).strength(0.4))
             .force('y', d3.forceY(function (d) {
-                return scales.y(d['defense'])
-            }).strength(0.99))
+                return height / 2
+            }).strength(0.4))
             .force('collide', d3.forceCollide(10))
-            .alphaDecay(0)
-            .alpha(0.12)
+            .alphaDecay(0.1)
+            .alpha(0.4)
             .on('tick', tick)
 
         let init_decay;
         init_decay = setTimeout(function () {
             console.log('init alpha decay')
-            simulation.alphaDecay(0.1);
-        }, 8000);
+            simulation.alphaDecay(0.01);
+        }, 4000);
+
+        // let collisionChange = document.getElementById('collision-change')
+        // collisionChange.addEventListener('input', e => {
+        //     simulation
+        //         .force('collide', d3.forceCollide(collisionChange.value))
+        //         .restart()
+        // })
 
         ///////////////////////
         // D3 NEW X-AXIS
         ///////////////////////
 
         dropdown.x.addEventListener('change', e => {
+
             axisFilter = dropdown.x.value
-            let scales = makeScales(axisFilter, data)
+            
+            if (axisFilter !== 'disabled') {
+                    
+                let scales = makeScales(axisFilter, data)
 
-            d3.select('.x-scale')
-                .transition()
-                .call(d3.axisBottom(scales.x))
+                d3.select('.x-scale')
+                    .transition()
+                    .duration(400)
+                    .call(d3.axisBottom(scales.x).tickSize(-(height - (padding * 2))))
+                    // .attr('fill', 'orangered')
+                    .attr('visibility', 'visible')
+                    .attr('stroke', 'orangered')
+                    .attr('opacity', 0.5)
+                        .transition()
+                        .duration(400)
+                        .attr('fill', 'none')
+                        .attr('stroke', 'none')
+                    
 
-            d3.select('.x-axis-label')
-                .transition()
-                .text(axisFilter.toUpperCase())
+                d3.select('.x-axis-label')
+                    .transition()
+                    .duration(200)
+                    .text(statMap[axisFilter].toUpperCase())
+                    .attr('visibility', 'visible')
+                    .attr('fill', 'orangered')
+                    .attr('stroke', 'orangered')
+                    .attr('stroke-width', '5px')
+                        .transition()
+                        .duration(200)
+                        .attr('fill', 'black')
+                        .attr('stroke-width', '0px')
 
-            simulation.force('x', d3.forceX(function (d) {
-                return scales.x(d[axisFilter])
-            }))
 
-            d3.selectAll('.circ')
-                .transition()
-                .attr('fill', (d) => { return scales.c(d[axisFilter]) })
-            // d3.selectAll('.sprite')
-            //     .transition()
-            //     .attr('width', (d) => { return scales.r(d[axisFilter]) })
-            //     .attr('height', (d) => { return scales.r(d[axisFilter]) })
+                simulation.force('x', d3.forceX(function (d) {
+                    return scales.x(d[axisFilter]) + scales.offsetScaleBandx
+                }))
 
-            simulation
-                .alphaDecay(0)
-                .alpha(0.12)
-                .restart()
+                d3.selectAll('.circ')
+                    .transition()
+                    .attr('fill', (d) => { return scales.c(d[axisFilter]) })
+                // d3.selectAll('.sprite')
+                //     .transition()
+                //     .attr('width', (d) => { return scales.r(d[axisFilter]) })
+                //     .attr('height', (d) => { return scales.r(d[axisFilter]) })
 
-            clearTimeout(init_decay);
+                simulation
+                    .alphaDecay(0)
+                    .alpha(0.4)
+                    .restart()
 
-            init_decay = setTimeout(function () {
-                console.log('init alpha decay');
-                simulation.alphaDecay(0.1);
-            }, 8000);
+                clearTimeout(init_decay);
+
+                init_decay = setTimeout(function () {
+                    console.log('init alpha decay');
+                    simulation.alphaDecay(0.1);
+                }, 4000);
+
+            } else {
+                d3.select('.x-scale')
+                    .transition(400)
+                    .attr('visibility', 'hidden')
+                d3.select('.x-axis-label')
+                    .transition(400)
+                    .attr('visibility', 'hidden')
+                simulation.force('x', d3.forceX(function (d) {
+                    return width / 2
+                }))
+            }
         })
 
         ///////////////////////
@@ -271,40 +350,70 @@ document.addEventListener('DOMContentLoaded', () => {
         ///////////////////////
 
         dropdown.y.addEventListener('change', e => {
-
+            
             axisFilter = dropdown.y.value
-            let scales = makeScales(axisFilter, data)
 
-            d3.select('.y-scale')
-                .transition()
-                .call(d3.axisLeft(scales.y))
+            if (axisFilter !== 'disabled') {
+                let scales = makeScales(axisFilter, data)
 
-            d3.select('.y-axis-label')
-                .transition()
-                .text(axisFilter.toUpperCase())
+                d3.select('.y-scale')
+                    .transition(400)
+                    .call(d3.axisLeft(scales.y).tickSize(-(width - (padding * 4))))
+                    // .attr('fill', 'rgba(0, 132, 255)')
+                    .attr('stroke', 'rgba(0, 132, 255)')
+                    .attr('opacity', 0.5)
+                    .transition()
+                    .duration(400)
+                    .attr('fill', 'none')
+                    .attr('stroke', 'none')
+                    .attr('visibility', 'visible')
 
-            d3.selectAll('.circ')
-                .transition()
-                .attr('fill', (d) => { return scales.c(d[axisFilter]) })
+                d3.select('.y-axis-label')
+                    .transition()
+                    .duration(200)
+                    .text(statMap[axisFilter].toUpperCase())
+                    .attr('fill', 'rgba(0, 132, 255)')
+                    .attr('stroke', 'rgba(0, 132, 255)')
+                    .attr('stroke-width', '5px')
+                    .transition()
+                    .duration(200)
+                    .attr('fill', 'black')
+                    .attr('stroke-width', '0px')
+                    .attr('visibility', 'visible')
 
-            simulation.force('y', d3.forceY(function (d) {
-                return scales.y(d[axisFilter])
-            }))
+                d3.selectAll('.circ')
+                    .transition()
+                    .attr('fill', (d) => { return scales.c(d[axisFilter]) })
 
-            simulation
-                .alphaDecay(0)
-                .alpha(0.12)
-                .restart()
+                simulation.force('y', d3.forceY(function (d) {
+                    return scales.y(d[axisFilter]) + scales.offsetScaleBandy
+                }))
 
-            clearTimeout(init_decay);
+                simulation
+                    .alphaDecay(0)
+                    .alpha(0.4)
+                    .restart()
 
-            init_decay = setTimeout(function () {
-                console.log('init alpha decay');
-                simulation.alphaDecay(0.1);
-            }, 8000);
+                clearTimeout(init_decay);
+
+                init_decay = setTimeout(function () {
+                    console.log('init alpha decay');
+                    simulation.alphaDecay(0.1);
+                }, 4000);
+            } else {
+                d3.select('.y-scale')
+                    .transition(400)
+                    .attr('visibility', 'hidden')
+                d3.select('.y-axis-label')
+                    .transition(400)
+                    .attr('visibility', 'hidden')
+                simulation.force('y', d3.forceY(function (d) {
+                    return height / 2
+                }))
+            }
         })
-
     })
+
 
 
 })
